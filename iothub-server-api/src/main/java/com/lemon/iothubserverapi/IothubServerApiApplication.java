2b588d4f4dfc670e159ed7f3592f873a81461a08
dengqiao <dengqiao@bmsmart.com.cn>
2020-01-08 10:36:37 +0800

    初次导入

---
+++ iothub-server-api/src/main/java/com/lemon/iothubserverapi/IothubServerApiApplication.java
@@ -0,0 +1,13 @@
+package com.lemon.iothubserverapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IothubServerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IothubServerApiApplication.class, args);
+    }
+
+}
